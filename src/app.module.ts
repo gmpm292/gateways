364aa3a4e57769f55c9ad60cf1f53d0a3f080f15
Carlos Perez <carlos.perez@thecodelight.com>
2023-03-17 17:42:11 -0400

    add typeORM

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 
 import { ConfigModule } from './common/config';
+import { DatabaseModule } from './common/database';
 import { GraphqlModule } from './common/graphql';
 import { AppInfoModule } from './modules/appInfo/appInfo.module';
 
 @Module({
-  imports: [AppInfoModule, ConfigModule, GraphqlModule],
+  imports: [AppInfoModule, ConfigModule, DatabaseModule, GraphqlModule],
 })
 export class AppModule {}
