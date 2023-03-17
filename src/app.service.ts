e299f54664bbe1d330f5a29cd3b28ad686a6d09b
Carlos Perez <carlos.perez@thecodelight.com>
2023-03-16 20:22:03 -0400

    golden de nuevo

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,16 @@
 import { Injectable } from '@nestjs/common';
+import { readFileSync } from 'fs';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  public appInfo() {
+    const { name, version, description } = JSON.parse(
+      readFileSync('package.json', 'utf8'),
+    );
+    return {
+      name,
+      version,
+      description,
+    };
   }
 }
