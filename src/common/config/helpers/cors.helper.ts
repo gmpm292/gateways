eb996b1cddb47032526e0743ab8ce7a967d631af
Carlos Perez <carlos.perez@thecodelight.com>
2023-03-17 11:00:58 -0400

    se borra todo

--- src/common/config/helpers/cors.helper.ts
+++
@@ -1,9 +0,0 @@
-/**
- * Returns an array with different origins configured in CORS_ORIGIN environment
- * variable
- */
-export function parseOriginFromEnvironment(): string[] {
-  const { CORS_ORIGIN } = process.env;
-
-  return CORS_ORIGIN?.split(',').map((origin: string) => origin.trim()) ?? [];
-}
--- src/common/errors/appErrors/NotFoundError.error.ts
+++
@@ -1,8 +0,0 @@
-import { AppError } from './app.error';
-import { AppErrorCode } from '../AppErrorCode.enum';
-
-export class NotFoundError extends AppError {
-  constructor() {
-    super({ code: AppErrorCode.NotFoundError, message: 'NotFoundError' });
-  }
-}
